b9a48f57eced6a3e94d3f4ea8685f1816decec20
Pavel Bozhinovski <147805085+Bozhinovski15@users.noreply.github.com>
2023-12-19 12:37:51 +0100

    Update CarApp.java

--- CarApp.java
+++ CarApp.java
@@ -1,9 +1,8 @@
-
 public class CarApp {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+    public static void main(String[] args) {
+        Car defaultCar = new Car();
+        Car mojaCar = new Car("Citroen", "C4", 2016);
+        System.out.println("Default kola : " + defaultCar);
+        System.out.println("Odbrana kola :  " + mojaCar);
+    }
 }
